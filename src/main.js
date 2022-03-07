978fc99accdb44a4bd53d395fc9b134fab18be0a
Florian Anderiasch <fa@art-core.org>
2022-03-07 01:37:34 +0100

    map

--- src/main.js
+++ src/main.js
@@ -1,37 +1,54 @@
 var Game = {
     display: null,
     map: {},
+    width: 160,
+    height: 80,
+    fontSize: 12,
+    bgDefault: "#757358",
+    numFish: 10,
+    fishSigil: "%",
+    fishColor: "#000",
     
     init: function() {
-        this.display = new ROT.Display();
+        var displayOpt = {width: this.width, height: this.height, fontSize: this.fontSize, bg: this.bgDefault};
+        this.display = new ROT.Display(displayOpt);
+        //this.display = new ROT.Display();
+        //console.debug(this.display.getContainer());
         document.body.appendChild(this.display.getContainer());
         
         this._generateMap();
     },
     
     _generateMap: function() {
-        var digger = new ROT.Map.Digger();
+        var map = new ROT.Map.Cellular(this.width, this.height, { connected: true });
         var freeCells = [];
-        
+        map.randomize(0.5);
+        for (var i=0; i<4; i++) map.create();
+
         var digCallback = function(x, y, value) {
             if (value) { return; }
-            
+            //console.debug("cb "+x+","+y+":"+value);
+
             var key = x+","+y;
-            this.map[key] = ".";
+            this.map[key] = " ";
             freeCells.push(key);
         }
-        digger.create(digCallback.bind(this));
-        
-        this._generateBoxes(freeCells);
-        
+
+        map.create(digCallback.bind(this));
+        //map.connect(this.display.DEBUG);
+        console.debug(map);
+
+        this._generateFish(freeCells);
         this._drawWholeMap();
+
     },
     
-    _generateBoxes: function(freeCells) {
-        for (var i=0;i<10;i++) {
+    _generateFish: function(freeCells) {
+        for (var i=0;i<this.numFish;i++) {
             var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
             var key = freeCells.splice(index, 1)[0];
-            this.map[key] = "*";
+            this.map[key] = this.fishSigil;
+            console.debug(key)
         }
     },
     
@@ -40,9 +57,17 @@ var Game = {
             var parts = key.split(",");
             var x = parseInt(parts[0]);
             var y = parseInt(parts[1]);
-            this.display.draw(x, y, this.map[key]);
+            var sigil = this.map[key];
+            // default: water
+            var fgc = "#fff";
+            var bgc = "#06c";
+            if (sigil == this.fishSigil) {
+                fgc = this.fishColor;
+            }
+            this.display.draw(x, y, sigil, fgc, bgc);
         }
     }
+
 };
 
-Game.init();
+//Game.init();

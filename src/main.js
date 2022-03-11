1a6b4742fefd2920d45cd5cd2a708a4ca6f5448c
Florian Anderiasch <fa@art-core.org>
2022-03-12 00:29:44 +0100

    intro panel

--- src/main.js
+++ src/main.js
@@ -405,20 +405,28 @@ var Game = {
             str += "<br/><br />";
             str += "This is about catching the biggest and rarest fish."
             str += "<br/>";
-            str += "To get there you will need to catch a lot of small ones (Icon Fish ) first."
-            str += "<br/>";
-            str += "This will allow you to pay toll to advance levels, maintain your energy (ico) and get stronger."
-            str += "<br/>";
-            str += "Never let your energy drop to 0, else you are done."
+            str += "To get there you will need to catch a lot of small ones"
+            str += "<img src='assets/t.gif' width='32' height='32' class='icon icon-fish' />";
+            str += "first.<br/>";
+            str += "This will allow you to pay toll to advance levels, maintain your energy"
+            str += "<img src='assets/t.gif' width='32' height='32' class='icon icon-energy' />";
+            str += "and get stronger.<br/>";
+            str += "Never let your energy drop to 0, else you lose."
             str += "<br/><br />";
 
-            str += "There will be predatory fish (icon predator) on each level who will eat other fish";
+            str += "There will be predatory fish";
+            str += "<img src='assets/t.gif' width='32' height='32' class='icon icon-pred' />";
+            str += "on each level who will eat other fish";
             str += "<br/>";
             str += "if you dont kill them with your Harpoon.",
             str += "<br />";
-            str += "There will also be a special fish (icon Miniboss) on every level with better rewards.",
+            str += "There will also be a special fish",
+            str += "<img src='assets/t.gif' width='32' height='32' class='icon icon-boss' />";
+            str += "on every level with better rewards.",
             str += "<br />";
-            str += "Search reed (Icon Reed) for extra treasure.",
+            str += "Search reed",
+            str += "<img src='assets/t.gif' width='32' height='32' class='icon icon-reed' />";
+            str += "for extra treasure.",
             str += "<br /><br />";
             str += "<strong>And enjoy your time fishing!</strong>",
             str += "<br /><br />";
